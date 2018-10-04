dde1fea9b8f55b0a03cd8a023d64cd69cce732f1
tomaslopez99 <tomy.lopez1999@gmail.com>
2018-10-04 15:14:33 -0300

    test

--- src/edu/austral/prog2_2018c2/Alien.java
+++ src/edu/austral/prog2_2018c2/Alien.java
@@ -3,7 +3,6 @@ package edu.austral.prog2_2018c2;
 import javax.swing.ImageIcon;
 
 public class Alien extends Sprite {
-
     private Bomb bomb;
     private final String alienImg = "src/images/alien.png";
 
---
+++ src/edu/austral/prog2_2018c2/Peter.java
@@ -0,0 +1,5 @@
+package edu.austral.prog2_2018c2;
+
+public class Peter {
+    int pedrito = 0;
+}
