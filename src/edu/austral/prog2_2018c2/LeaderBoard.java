8dac7304bb89497e8bb556c3874bf00a565216e1
alejo <alejoelectronico1@gmail.com>
2018-10-20 22:54:26 -0300

    LeaderBoard is now permanently sorted

--- src/edu/austral/prog2_2018c2/LeaderBoard.java
+++ src/edu/austral/prog2_2018c2/LeaderBoard.java
@@ -1,6 +1,7 @@
 package edu.austral.prog2_2018c2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LeaderBoard { //Podriamos hacer que este objeto sea un metodo estatico en Score que devuelva una lista de scores si no le encontramos un sentido al objeto leaderboard
@@ -8,7 +9,14 @@ public class LeaderBoard { //Podriamos hacer que este objeto sea un metodo estat
     private List<Score> scores;
 
     public LeaderBoard(){
-        scores = new ArrayList<Score>(); //Podriamos hacer una Sorted List y sortearla por el int de scores pero para eso hay q hacer algo de un comparator
+        scores = new ArrayList<Score>() {//Aca armo una lista que se mantiene a si misma ordenada por puntaje del Score
+            @Override
+            public boolean add(Score score){
+                super.add(score);
+                Collections.sort(this, (Score::compareTo));
+                return true;
+            }
+        };
     }
 
     public void addScore(Score score){
